415e61a4e3b0f06d2744e5a7cc863036b1a6081e
Hunter <yuanxy92@users.noreply.github.com>
2024-09-26 12:50:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from UVCCamera import *
+import winsound
 
 def main():
-    cam_num = 3
-    fps = 15
-    frame_num = 2000
-    out_dir = './data/data_0823_6'
+    cam_num = 6
+    fps = 10
+    frame_num = 900
+    out_dir = './data/data_0829_10'
     os.makedirs(out_dir, exist_ok=True)
     
     # init parallel camera using ray  
+    cam_indices = [0, 1, 2, 4, 5, 6]
     ray.init()
     sync_signal = SyncSignal.remote()
-    workers = [UVCCamera.remote(i+1, frame_num) for i in range(cam_num)]
+    workers = [UVCCamera.remote(cam_indices[i], frame_num) for i in range(cam_num)]
     res = ray.get([workers[i].init_camera.remote() for i in range(cam_num)])
     print(res)
     processes = [workers[i].capture_soft_sync.remote(sync_signal) for i in range(cam_num)]
     
     # generate ray signals to capture images
     print('Images capturing start in 3 seconds')
+    winsound.Beep(600, 500)
     time.sleep(1)
     print('Images capturing start in 2 seconds')
+    winsound.Beep(600, 500)
     time.sleep(1)
     print('Images capturing start in 1 seconds ')
+    winsound.Beep(600, 500)
     time.sleep(1)
     print('Capturing ... ')
     
@@ -38,6 +43,7 @@ def main():
     print(ray.get([workers[i].save_img.remote(out_dir) for i in range(cam_num)]))
 
     print('Images saved!')
+    winsound.Beep(600, 2000)
 
 if __name__ == "__main__":
     main()
